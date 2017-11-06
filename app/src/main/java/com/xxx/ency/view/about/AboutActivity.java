3bd13c98fa7929c3161cf1e9b7d951ca1be9177d
xiarh <xiarunhao123@163.com>
2017-11-06 17:22:46 +0800

    可伸缩Toolbar，并且可沉浸式

--- app/src/main/java/com/xxx/ency/view/about/AboutActivity.java
+++ app/src/main/java/com/xxx/ency/view/about/AboutActivity.java
@@ -1,5 +1,13 @@
 package com.xxx.ency.view.about;
 
+import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.xxx.ency.R;
 import com.xxx.ency.base.BaseMVPActivity;
 import com.xxx.ency.config.EncyApplication;
@@ -9,6 +17,8 @@ import com.xxx.ency.di.module.AboutActivityModule;
 import com.xxx.ency.model.bean.BingBean;
 import com.xxx.ency.presenter.BingPresenter;
 
+import butterknife.BindView;
+
 /**
  * 关于界面
  * Created by xiarh on 2017/11/3.
@@ -16,6 +26,15 @@ import com.xxx.ency.presenter.BingPresenter;
 
 public class AboutActivity extends BaseMVPActivity<BingPresenter> implements BingContract.View {
 
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.img_about)
+    ImageView mImgAbout;
+
+    private RequestOptions options = new RequestOptions()
+            .centerCrop()
+            .priority(Priority.IMMEDIATE)
+            .diskCacheStrategy(DiskCacheStrategy.NONE);
 
     @Override
     protected int getLayoutId() {
@@ -34,11 +53,17 @@ public class AboutActivity extends BaseMVPActivity<BingPresenter> implements Bin
 
     @Override
     protected void initialize() {
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("关于");
         mPresenter.getData();
     }
 
     @Override
     public void showBingBean(BingBean bingBean) {
-        showMsg("success");
+        Glide.with(mContext)
+                .applyDefaultRequestOptions(options)
+                .load(bingBean.getData().getBmiddle_pic())
+                .into(mImgAbout);
     }
 }
