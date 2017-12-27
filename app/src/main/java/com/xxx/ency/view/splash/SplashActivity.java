ff37d881910fa141b6e6e29b7c33faa3dc79de8a
xiarh <xiarunhao123@163.com>
2017-12-27 11:07:34 +0800

    新增启动页

---
+++ app/src/main/java/com/xxx/ency/view/splash/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.xxx.ency.view.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+import com.xxx.ency.view.main.MainActivity;
+
+import me.yokeyword.fragmentation.SupportActivity;
+
+/**
+ * 启动页
+ * Created by xiarh on 2017/12/27.
+ */
+
+public class SplashActivity extends SupportActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        }, 1000);
+    }
+}
